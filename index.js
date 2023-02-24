294df0603d996fe53ac6fc897560b873b4366e1c
Talla Giridhar <giridhar.talla2002@gmail.com>
2023-02-24 12:29:06 +0000

    add format + lint

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const app = express()
 const PORT = process.env.PORT | 5000
 
 app.get('/', (req, res) => {
-	res.json({
-		name: 'home',
-	})
+  res.json({
+    name: 'home',
+  })
 })
 
 app.get('/deekshith', (req, res) => {
-	res.json({
-		name: 'deekshith',
-	})
+  res.json({
+    name: 'deekshith',
+  })
 })
 
 app.listen(PORT, () => {
-	console.log('server started listening at port: ', PORT)
+  console.log('server started listening at port: ', PORT)
 })
