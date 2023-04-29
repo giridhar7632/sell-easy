168e2b8d55417e534429257821f8e1d6d7359c6b
Talla Giridhar <giridhar.talla2002@gmail.com>
2023-04-29 13:25:12 +0530

    add node engine

--- client/src/components/common/Button.js
+++ client/src/components/common/Button.js
@@ -12,7 +12,9 @@ export default function Button({
   const variantClassname = clsx({
     ['bg-teal-500 hover:bg-teal-600 text-white disabled:bg-teal-500 disabled:ring-0']:
       variant === 'primary',
-    ['text-teal-500 disabled:text-teal-500 bg-white bg-opacity-10 backdrop-blur-sm']:
+    ['bg-teal-100 ring-teal-50 text-teal-500 hover:text-teal-600 disabled:bg-teal-100 disabled:ring-0']:
+      variant === 'secondary',
+    ['text-teal-500 disabled:text-teal-300 bg-white bg-opacity-10 backdrop-blur-sm']:
       variant === 'text',
   })
 
